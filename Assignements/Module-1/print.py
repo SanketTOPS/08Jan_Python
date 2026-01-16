1bc195f6db9fe08280edae6c485688a6009a3ea4
SanketTops <sanket.tops@gmail.com>
2026-01-16 13:04:10 +0530

    Module-1

---
+++ Assignements/Module-1/print.py
@@ -0,0 +1,7 @@
+#print("Hello Students!")
+
+"""print('Welcome to Python Programming.')
+print('''Hello Python!
+      hruwiuerwr
+      reowiorwoer
+      roweroiweo''')"""
---
+++ Assignements/Module-1/var.py
@@ -0,0 +1,15 @@
+"""a=34
+b=65
+
+#print(a+b)
+print("Sum is:", a+b)"""
+
+
+# --------------------- #
+"""name="Sanket"
+print("My name is ",name)"""
+
+# ----------------------- #
+
+pi=3.14
+print("Value of PI:",pi)
