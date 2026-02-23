24082dba5cec62a18433a8c76c1cdb4cd1aa59d0
SanketTops <sanket.tops@gmail.com>
2026-02-23 13:27:06 +0530

    Exception

---
+++ Assignements/Module-3_Exceptions/try_demo.py
@@ -0,0 +1,7 @@
+a=34
+b=56
+print("Sum:",A+b)
+
+x=65
+y=67
+print("Mul:",x*y)
---
+++ Assignements/Module-3_Exceptions/try_except.py
@@ -0,0 +1,12 @@
+try:
+    a=34
+    b=65
+    print("Sum:",A+b)
+except:
+    print("Error!")
+
+x=34
+y=87
+print("Mul:",x*y)
+    
+    
